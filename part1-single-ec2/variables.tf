209cb3db5f4b006b5fb9f128a394ab5dbe61f102
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:30:02 +0530

    Update variables.tf

--- part1-single-ec2/variables.tf
+++ part1-single-ec2/variables.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "vidit-key"
+}
+
+# part1-single-ec2/outputs.tf
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
