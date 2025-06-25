937bba7d14b1d4e972ad09cbd710139e083ace56
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:51:16 +0530

    Update ecr.tf

--- part3-ecs-docker/terraform/ecr.tf
+++ part3-ecs-docker/terraform/ecr.tf
@@ -1,1 +1,15 @@
+resource "aws_ecr_repository" "flask" {
+  name                 = "flask-backend"
+  image_tag_mutability = "MUTABLE"
+  tags = {
+    Name = "Flask ECR Repo"
+  }
+}
 
+resource "aws_ecr_repository" "express" {
+  name                 = "express-frontend"
+  image_tag_mutability = "MUTABLE"
+  tags = {
+    Name = "Express ECR Repo"
+  }
+}
