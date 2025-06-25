38c3d2d7cf73a2a1232efd0f16de58d5a0faff98
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:47:53 +0530

    Update main.tf

--- part3-ecs-docker/terraform/main.tf
+++ part3-ecs-docker/terraform/main.tf
@@ -1,1 +1,19 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "./vpc.tf"
+}
+
+module "ecr" {
+  source = "./ecr.tf"
+}
+
+module "ecs" {
+  source = "./ecs.tf"
+}
+
+module "alb" {
+  source = "./alb.tf"
+}
