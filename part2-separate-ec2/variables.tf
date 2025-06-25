a990466a1dbe308b94df258572499b59365c3eee
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:38:53 +0530

    Update variables.tf

--- part2-separate-ec2/variables.tf
+++ part2-separate-ec2/variables.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "vidit-key"
+}
