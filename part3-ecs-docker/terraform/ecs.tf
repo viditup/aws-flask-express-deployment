d29241fa2208d8fa9bb7d07e8304941eca54095e
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:49:51 +0530

    Update ecs.tf

--- part3-ecs-docker/terraform/ecs.tf
+++ part3-ecs-docker/terraform/ecs.tf
@@ -1,1 +1,73 @@
+resource "aws_ecs_cluster" "main" {
+  name = "flask-express-cluster"
+}
 
+resource "aws_ecs_task_definition" "flask" {
+  family                   = "flask-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "flask"
+      image     = var.flask_image
+      portMappings = [{ containerPort = 5000 }]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "express" {
+  family                   = "express-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "express"
+      image     = var.express_image
+      portMappings = [{ containerPort = 3000 }]
+    }
+  ])
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
