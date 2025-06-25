2ef7fa74af21fb382a71f2b81229fa2545d9ba0a
Vidit Upadhyay <117540681+viditup@users.noreply.github.com>
2025-06-25 13:49:17 +0530

    Update alb.tf

--- part3-ecs-docker/terraform/alb.tf
+++ part3-ecs-docker/terraform/alb.tf
@@ -1,1 +1,36 @@
+resource "aws_lb" "app_lb" {
+  name               = "ecs-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public[*].id
+}
+
+resource "aws_lb_target_group" "flask_tg" {
+  name     = "flask-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group" "express_tg" {
+  name     = "express-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default backend"
+      status_code  = "200"
+    }
+  }
+}
 
